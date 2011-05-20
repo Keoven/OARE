797c10a2d067168d09bdb56c55c3f71cb3af575d
Nelvin Driz <ndriz@exist.com>
2011-05-20 13:49:51 +0800

    Fixed Bugs in Nested Attributes Error

--- lib/active_resource_override/errors.rb
+++ lib/active_resource_override/errors.rb
@@ -4,13 +4,15 @@ class Oare::Errors < ActiveResource::Errors
     humanized_nested_attributes = @base.nested_attributes_values.map do |model, v|
       v.map do |i, v2|
         v2.keys.map do |k|
-          "#{model.gsub('_attributes','')} #{k}".humanize
+          attribute_name = "#{model.gsub('_attributes','')}_#{k}".underscore
+          [attribute_name.humanize, attribute_name]
         end
       end
     end.flatten
+    humanized_nested_attributes = Hash[*humanized_nested_attributes]
 
     humanized_attributes = @base.attributes.keys.inject({}) { |h, attr_name| h.update(attr_name.humanize => attr_name) }
-    humanized_attributes += humanized_nested_attributes
+    humanized_attributes.merge!(humanized_nested_attributes)
     messages.each do |message|
       attr_message = humanized_attributes.keys.detect do |attr_name|
         if message[0, attr_name.size + 1] == "#{attr_name} "
--- lib/oare.rb
+++ lib/oare.rb
@@ -2,6 +2,10 @@ module Oare
 end
 
 if defined?(Rails) && Rails::VERSION::MAJOR == 3
+  require 'active_resource'
+  require 'active_resource/base'
+  require 'active_resource/validations'
+
   require File.expand_path('active_resource_override/connection', File.dirname(__FILE__))
   require File.expand_path('active_resource_override/errors'    , File.dirname(__FILE__))
   require File.expand_path('active_resource_override/base'      , File.dirname(__FILE__))
